2c46b4d4d6750d480906e63f3f586184194c420d
Nihar Panigrahi <nihar108@gmail.com>
2018-07-25 21:20:07 +0530

    add codepush to App

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import CodePush from 'react-native-code-push';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,7 +18,7 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
@@ -28,6 +29,9 @@ export default class App extends Component<Props> {
     );
   }
 }
+
+App = CodePush(App);
+export default App;
 
 const styles = StyleSheet.create({
   container: {
